4ba57c211b487774cdd5316496748f2dc29e9450
Imam Candra Kusuma <icekacandra@gmail.com>
2020-03-22 00:27:56 +0700

    Add Settings

--- app/src/main/java/com/iceka/earthquakereport/MainActivity.java
+++ app/src/main/java/com/iceka/earthquakereport/MainActivity.java
@@ -1,11 +1,22 @@
 package com.iceka.earthquakereport;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -24,10 +35,13 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private RecyclerView mRecyclerView;
     private List<Earthquake> earthquakes = new ArrayList<>();
+    private EarthquakeAdapter adapter;
+    private TextView mEmptyEarthquake;
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRecyclerView = findViewById(R.id.rv_earthquake);
+        mEmptyEarthquake = findViewById(R.id.empty_view);
+        mLoadingIndicator = findViewById(R.id.loading_indicator);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         mRecyclerView.setHasFixedSize(true);
@@ -42,18 +58,30 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL));
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        retrieveData();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
 
         AndroidNetworking.initialize(getApplicationContext());
 
-
+        retrieveData();
     }
 
     private void retrieveData() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String minMagnitude = sharedPreferences.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+
+        String orderBy = sharedPreferences.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
         AndroidNetworking.get(Constant.BASE_URL)
                 .addQueryParameter("format", "geojson")
-                .addQueryParameter("minmagnitude", "6")
                 .addQueryParameter("limit", "10")
+                .addQueryParameter("orderby", orderBy)
+                .addQueryParameter("minmagnitude", minMagnitude)
                 .setTag("TESTING")
                 .build()
                 .getAsJSONObject(new JSONObjectRequestListener() {
@@ -61,17 +89,24 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         try {
                             JSONArray features = response.getJSONArray("features");
-                            for (int i = 0; i < features.length(); i++) {
-                                JSONObject current = features.getJSONObject(i);
-                                JSONObject properties = current.getJSONObject("properties");
-                                earthquakes.add(new Earthquake(
-                                        properties.getDouble("mag"),
-                                        properties.getString("place"),
-                                        properties.getLong("time"),
-                                        properties.getString("url")
-                                ));
+                            if (features.length() > 0) {
+                                for (int i = 0; i < features.length(); i++) {
+                                    JSONObject current = features.getJSONObject(i);
+                                    JSONObject properties = current.getJSONObject("properties");
+                                    earthquakes.add(new Earthquake(
+                                            properties.getDouble("mag"),
+                                            properties.getString("place"),
+                                            properties.getLong("time"),
+                                            properties.getString("url")
+                                    ));
+                                }
+                                mEmptyEarthquake.setVisibility(View.GONE);
+                            } else {
+                                mEmptyEarthquake.setVisibility(View.VISIBLE);
+                                mEmptyEarthquake.setText("Tidak ada Data");
                             }
-                            EarthquakeAdapter adapter = new EarthquakeAdapter(getApplicationContext(), earthquakes);
+                            mLoadingIndicator.setVisibility(View.GONE);
+                            adapter = new EarthquakeAdapter(getApplicationContext(), earthquakes);
                             mRecyclerView.setAdapter(adapter);
                             adapter.setItemClickListener(onItemClickListener);
                         } catch (JSONException e) {
@@ -95,4 +130,32 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(MainActivity.this, "You Clicked: " + earthquake.getMagnitude(), Toast.LENGTH_SHORT).show();
     };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_settings) {
+            startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.settings_min_magnitude_key)) || key.equals(getString(R.string.settings_order_by_key))) {
+            earthquakes.clear();
+
+            mEmptyEarthquake.setVisibility(View.GONE);
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/iceka/earthquakereport/SettingsActivity.java
@@ -0,0 +1,62 @@
+package com.iceka.earthquakereport;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.settings_main, rootKey);
+
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
+
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+
+    }
+
+}
